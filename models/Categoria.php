5838e59fae5849f3fee10a8287b258e44b50c992
Marlon Vásquez H <mvhsiesa@gmail.com>
2020-11-20 11:14:57 -0600

    Mejoras en funcionalidad de categorías

---
+++ models/Categoria.php
@@ -0,0 +1,76 @@
+<?php
+class Categoria
+{
+
+    private $id;
+    private $nombre;
+    private $db;
+
+
+    public function __construct()
+    {
+        $this->db = Database::connect();
+    }
+
+
+    /**
+     * Get the value of id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of nombre
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * Set the value of nombre
+     *
+     * @return  self
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $this->db->real_escape_string($nombre);
+
+        return $this;
+    }
+
+
+    public function getAll()
+    {
+        $categorias = $this->db->query("SELECT * FROM categorias ORDER BY id DESC;");
+        return $categorias;
+    }
+
+    public function save()
+    {
+        $nombre = $this->getNombre();
+
+        $sql = "INSERT INTO categorias VALUES(NULL, '$nombre')";
+        $guardar = $this->db->query($sql);
+
+        $result = false;
+        if ($guardar) {
+            $result = true;
+        }
+        return $result;
+    }
+}
--- views/layout/header.php
+++ views/layout/header.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tienda camisetas</title>
-    <link rel="stylesheet" href="<?=base_url?>assets/css/styles.css">
+    <link rel="stylesheet" href="<?= base_url ?>assets/css/styles.css">
 </head>
 
 <body>
@@ -14,7 +14,7 @@
         <!-- CABECERA-->
         <header id="header">
             <div id="logo">
-                <img src="<?=base_url?>assets/img/camiseta.png" alt="Camiseta Logo" />
+                <img src="<?= base_url ?>assets/img/camiseta.png" alt="Camiseta Logo" />
                 <a href="index.php">
                     Tienda de Camisetas
                 </a>
@@ -22,23 +22,17 @@
             </div>
         </header>
         <!-- MENU -->
+        <?php $categorias = Utils::showCategorias(); ?>
         <nav id="menu">
             <ul>
                 <li>
                     <a href="">Inicio</a>
                 </li>
-                <li>
-                    <a href="">Categoría 1</a>
-                </li>
-                <li>
-                    <a href="">Categoría 2</a>
-                </li>
-                <li>
-                    <a href="">Categoría 3</a>
-                </li>
-                <li>
-                    <a href="">Categoría 4</a>
-                </li>
+                <?php while ($cat = $categorias->fetch_object()) : ?>
+                    <li>
+                        <a href="#"><?=$cat->nombre?></a>
+                    </li>
+                <?php endwhile; ?>
             </ul>
         </nav>
 
